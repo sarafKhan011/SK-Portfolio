9bfd29f112f32cd55cdb166cfc499e375eb4edfc
sarafKhan011 <sarafkhanmohammed@gmail.com>
2025-08-02 14:00:54 +0530

    Update: added new CV

--- src/components/aboutMeSection/AboutMeText.jsx
+++ src/components/aboutMeSection/AboutMeText.jsx
@@ -14,7 +14,7 @@ const AboutMeText = () => {
       </p>
 
       <a
-        href="/SarafKhan.pdf"
+        href="/SarafK.pdf"
         download
         className="mt-10 flex items-center gap-2 border border-orange text-white rounded-full py-2 px-5 hover:bg-orange hover:text-white transition-all duration-300"
       >
